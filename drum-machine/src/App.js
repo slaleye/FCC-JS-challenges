550649883396241819c38f532557096598ab9caf
Salomon Laleye <salomonlaleye@gmail.com>
2019-09-24 08:39:53 +0200

    key not showing

--- drum-machine/src/App.js
+++ drum-machine/src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import Display from './components/Display';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div id="drum-machine" className="App">
+        <h3>Drum Machine</h3>
+        <Display />
+      </div>
+    );
+  }
 }
 
 export default App;
