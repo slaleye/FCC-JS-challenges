15cb08604fa89184d56a4aa16578293a70e46fcd
Salomon Laleye <salomonlaleye@gmail.com>
2019-09-26 11:17:00 +0200

    removed javascript-calculator folder

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+import './App.css';
+import Display from './components/Display';
+
+function App() {
+  return (
+    <div id="drum-machine" className="App">
+       <h3>Drum Machine</h3>
+       <Display />
+    </div>
+  );
+}
+
+export default App;
